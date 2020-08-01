dd4e658dd19adb32197f644e0969bc36d67f229c
“wu.qinghua <wu.qinghua@jianpeicn.com>
2020-08-01 17:04:29 +0800

    提交api接口

---
+++ src/Lx.Domain/Models/SystemManager/MerchantsAccount.cs
@@ -0,0 +1,51 @@
+﻿using Lx.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Lx.Domain.Models.SystemManager
+{
+    /// <summary>
+    /// 商家账户
+    /// </summary>
+    [Table("LX_MERCHANTS_ACCOUNT")]
+    public class MerchantsAccount : Entity
+    {
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        public string NickName { get; private set; }
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string Phone { get; private set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        public string Email { get; private set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string PassWord { get; private set; }
+        /// <summary>
+        /// 头像
+        /// </summary>
+        public string Avatar { get; private set; }
+        /// <summary>
+        /// 注册时间
+        /// </summary>
+        public DateTime CreateTime { get; private set; }
+
+        public MerchantsAccount()
+        {
+
+        }
+
+        public MerchantsAccount(string nickName,string phone,string email,string passWord,string avatar,DateTime createTime)
+        {
+            this.NickName = nickName;
+            this.Phone = phone;
+        }
+    }
+}
---
+++ src/Lx.Domain/Models/SystemManager/MerchantsRole.cs
@@ -0,0 +1,40 @@
+﻿using Lx.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Lx.Domain.Models.SystemManager
+{
+    /// <summary>
+    /// 商家角色
+    /// </summary>
+    [Table("LX_MERCHANTS_ROLE")]
+    public class MerchantsRole :  Entity
+    {
+        /// <summary>
+        /// 角色名
+        /// </summary>
+        public string RoleName { get; private set; }
+        /// <summary>
+        /// 是否管理员
+        /// </summary>
+        public bool IsAdmin { get; private set; }
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        public bool IsUse { get; private set; }
+
+        public MerchantsRole()
+        {
+
+        }
+
+        public MerchantsRole(string roleName, bool isAdmin, bool isUse)
+        {
+            this.RoleName = roleName;
+            this.IsAdmin = IsAdmin;
+            this.IsUse = IsUse;
+        }
+    }
+}
